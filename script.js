d6ac734eb67c29a892889351caadca9f2629693a
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2022-10-15 20:06:22 -0400

    set up board

---
+++ script.js
@@ -0,0 +1,23 @@
+
+window.onload = function(){
+function myArray(){
+    console.log('test2');
+
+
+    let myInput = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+}
+
+myArray()
+
+
+
+document.getElementById("one").onclick = function(event){
+    console.log("hello world")
+    document.getElementById("one").innerHTML = 'x'
+    }
+
+}
